751744a2e0736c042358272bfd4c6b49a0dc136e
dcauwe <dorian8080@gmail.com>
2023-10-15 22:11:52 -0600

    Add basic main.c structure

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#define LED0_NODE DT_ALIAS(led0)
+
+#define SLEEP_TIME_MS 200
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 int main() {
-    
+    int ret;
+
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
 }
