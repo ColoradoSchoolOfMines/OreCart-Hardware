3cb1ec19455daccd9eb534c3fb00a99f7e6641ba
dcauwe <dorian8080@gmail.com>
2023-10-26 15:31:25 -0600

    Add connection to the LTE network

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#include "modules/server_module/nrf9160_setup.h"
+
 #define LED0_NODE DT_ALIAS(led0)
 
 #define SLEEP_TIME_MS 2000
@@ -8,6 +10,7 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main() {
+	printk("Welcome to the OreCart Hardware Project!\r\n");
     int ret;
 
     if (!gpio_is_ready_dt(&led)) {
@@ -19,11 +22,14 @@ int main() {
 		return 0;
 	}
 
-	const struct device *dev;
-	uint32_t dtr = 0;
+#if defined(CONFIG_NRF_MODEM_LIB)
+	init_nrf9160_modem();
+#endif
 
 	while (1) {
 		k_sleep(K_SECONDS(1));
+		// printk("Beep\r\n");
+		gpio_pin_toggle_dt(&led);
 	}
 
 	return 0;
