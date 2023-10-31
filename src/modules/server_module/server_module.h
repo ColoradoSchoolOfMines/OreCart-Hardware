5c9d9aa47154e3dd7b4f8f03d16ace3097b88275
dcauwe <dorian8080@gmail.com>
2023-10-31 16:51:51 -0600

    Finish basic architecture

--- src/modules/server_module/server_module.h
+++ src/modules/server_module/server_module.h
@@ -4,11 +4,26 @@
 
 #include "../../common/location.h"
 
+#define SERVER_HOST "<URL>"
+#define PROTOCOL "HTTP"
+
+
+#define SEND_VAN_LOCATION_ROUTE "/location/"
+#define SEND_OCCUPANCY_ROUTE "/states/ridership/"
+
+
 // Lock semaphore until initialized.
 K_SEM_DEFINE(modem_available, 0, 1);
 
-void server_send_van_location(struct Location location, double ts);
+void server_module_init();
 
-void server_send_ridership(double ts);
+// Send the current van location to the server (blocking).
+// @location   -  a lat/long position of the van
+// returns success boolean
+bool server_send_van_location(VanInfo* vanInfo, struct Location location, uint64_t ts);
 
-k_msgq_init(&my_msgq, my_msgq_buffer, sizeof(struct data_item_type), 10);
+// Send the current ridership to the server (blocking).
+// @ridership_delta   -   how many people got on/off the bus, negative for off and positive for on.
+// returns success boolean
+bool server_send_ridership(VanInfo* vanInfo, int8_t ridership_delta, uint64_t ts);
+
