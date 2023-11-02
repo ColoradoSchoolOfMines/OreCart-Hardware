a8a044272737be9a7a9dc9f52221585185aaf57f
dcauwe <dorian8080@gmail.com>
2023-11-02 11:03:42 -0600

    Remove extra build directory

--- build_1/zephyr/zephyr.dts.d
+++
@@ -1,7 +0,0 @@
-empty_file.o: /home/dorian/ncs/v2.4.2/zephyr/misc/empty_file.c \
- /home/dorian/ncs/v2.4.2/zephyr/boards/posix/native_posix/native_posix.dts \
- /home/dorian/ncs/v2.4.2/zephyr/dts/posix/posix.dtsi \
- /home/dorian/ncs/v2.4.2/zephyr/dts/common/skeleton.dtsi \
- /home/dorian/ncs/v2.4.2/zephyr/dts/common/mem.h \
- /home/dorian/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
- /home/dorian/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/gpio/gpio.h
