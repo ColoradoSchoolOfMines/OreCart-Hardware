c0659a23dc0c291461bdb55497babd6bb8edbac6
dcauwe <dorian8080@gmail.com>
2024-01-20 10:36:58 -0700

    Access screen from DT

--- src/modules/display_module/display_test.cpp
+++ src/modules/display_module/display_test.cpp
@@ -2,36 +2,39 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/display.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/spi.h>
 
-uint8_t my_buffer[4] = {0};
-struct spi_buf my_spi_buffer[1];
-my_spi_buffer[0].buf = my_buffer;
-my_spi_buffer[0].len = 4;
-const struct spi_buf_set rx_buff = { my_spi_buffer, 1 };
+// uint8_t my_buffer[4] = {0};
+// struct spi_buf my_spi_buffer[1];
+// my_spi_buffer[0].buf = my_buffer;
+// my_spi_buffer[0].len = 4;
+// const struct spi_buf_set rx_buff = { my_spi_buffer, 1 };
 
-const struct device *gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-const struct spi_dt_spec display_waveshare = SPI_DT_SPEC_GET(DT_NODELABEL(waveshare), SPI_OP, 0);
+// const struct device *gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+// const struct spi_dt_spec display_waveshare = SPI_DT_SPEC_GET(DT_NODELABEL(waveshare), SPI_OP, 0);
 
 #define BL_PIN 15
 #define RST_PIN 16
 #define DC_PIN 17
 
 void display_test() {
-    /* Gpio pin */
-    if (!device_is_ready(gpio))
-        printk("Failed to get the gpio0 device");
+    const device* display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    display_capabilities capabilities;
 
-    gpio_pin_configure(gpio, BL_PIN, GPIO_OUTPUT_HIGH);
-    gpio_pin_configure(gpio, RST_PIN, GPIO_OUTPUT_LOW);
-    gpio_pin_configure(gpio, DC_PIN, GPIO_OUTPUT_LOW);
+	if (!device_is_ready(display_dev)) {
+		printk("Device %s not found. Aborting.", display_dev->name);
+        return;
+    }
 
-    // while (true) {
-    //     gpio_pin_toggle(gpio, BL_PIN);
-    //     k_sleep(K_SECONDS(2));
-    // }
+    display_get_capabilities(display_dev, &capabilities);
 
-
-    spi_write_dt(&display_waveshare);
+    switch (capabilities.current_pixel_format) {
+        case PIXEL_FORMAT_RGB_888:
+            printk("PIXEL_FORMAT_RGB_888\r\n");
+            break;
+        case PIXEL_FORMAT_MONO01:
+            printk("PIXEL_FORMAT_MONO01\r\n");
+    }
 }
